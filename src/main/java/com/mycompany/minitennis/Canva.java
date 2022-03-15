5ea8f9fc01639223d3993a93b964bf33ff9debe6
he_fa <he_fa@acero-laptop>
2022-03-14 20:22:12 -0400

    dibuja una linea

---
+++ src/main/java/com/mycompany/minitennis/Canva.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.minitennis;
+
+/**
+ *
+ * @author he_fa
+ */
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Graphics;
+public class Canva extends JPanel{
+    public Canva(){
+        this.setPreferredSize(new Dimension(300,300));
+    }
+    private void draw(Graphics g){
+        g.drawLine(0,0,150,100);
+    }
+    @Override
+    protected void paintComponent(Graphics g){
+        draw(g);
+    }
+}
