38db5872026582c80a2e768d971763a5c76e07be
he_fa <he_fa@acero-laptop>
2022-03-14 20:33:44 -0400

    ventana

---
+++ src/main/java/com/mycompany/minitennis/MainWindow.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.minitennis;
+
+/**
+ *
+ * @author he_fa
+ */
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+    public MainWindow(){
+               
+        setVisible(true);
+    }
+  
+    public static void main(String... args){
+        MainWindow ventana = new MainWindow();
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Creacion
+        Canva canva = new Canva();
+        //Asignacion
+        ventana.setContentPane(canva);
+        ventana.pack();
+        //Localizacion
+        ventana.setLocation(200,200);
+        
+    }
+}
